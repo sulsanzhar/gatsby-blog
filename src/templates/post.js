4776dfca25dfb2f8e3dcf2514245b39ffbb16dc4
Sultanov Sanzhar <sulsanzhar@mail.ru>
2025-06-29 23:53:25 +0500

    added post.js to templates folder

---
+++ src/templates/post.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+export default function Post({ data }) {
+  const post = data.markdownRemark
+  return (
+    <div>
+      <h1>{post.frontmatter.title}</h1>
+      <p>{post.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  )
+}
